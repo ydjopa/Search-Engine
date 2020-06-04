64615386dc07d04c5aefb548238b176439e244bc
ydjopa <ydjopa@gmail.com>
2020-06-04 11:18:43 -0400

    fixed css media first

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -1,10 +1,53 @@
 "use strict";
 
-// Replace ./data.json with your JSON feed
-fetch('https://covidtracking.com/api/v1/states/current.json').then(function (response) {
-  return response.json();
-}).then(function (data) {
-  // Work with JSON data here
-  console.log(state.data);
-})["catch"](function (err) {// Do something for an error here
-});
+var url = 'https://covidtracking.com/api/v1/states/current.json';
+var inputRandom = "WHAT";
+var inputDate = "DATE";
+var inputState = "STATE";
+$(document).ready(function () {});
+var array = new Array();
+var arrayImages = new Array();
+
+function getData() {
+  var e = document.getElementById("dropdown");
+  var states = e.options[e.selectedIndex].value;
+
+  if (array.length > 0) {
+    for (i = 0; i < array.length; i++) {
+      if (array[i].state == states) {
+        $('.card-title').html("<h3> Positives are " + array[i].state + " " + array[i].positive + "</h3>");
+        $('.card-body').html("<p class='lead'> " + array[i].lastUpdateEt + "</p>");
+      }
+    }
+
+    for (i = 0; i < arrayImages.length; i++) {
+      if (arrayImages[i].state == states) {
+        $('.card-body').html("<img class='img-fluid' src='" + arrayImages[i].url + "'/>");
+      }
+    }
+  } else {
+    $.get('https://covidtracking.com/api/v1/states/current.json', function (response) {
+      array = response;
+      var output = '';
+      var obj;
+
+      for (i = 0; i < response.length; i++) {
+        if (response[i].state == states) {
+          $('.card-title').html("<h3> Positives are " + response[i].state + " " + response[i].positive + "</h3>");
+          $('.card-footer').html("<p class='lead'> " + response[i].lastUpdateEt + "</p>");
+        }
+      }
+    });
+    $.get('https://covidtracking.com/api/v1/states/screenshots.json', function (response) {
+      arrayImages = response;
+      var output = '';
+      var obj;
+
+      for (i = 0; i < response.length; i++) {
+        if (response[i].state == states) {
+          $('.card-body').html("<img class='img-fluid' src='" + response[i].url + "'/>");
+        }
+      }
+    });
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,38 +1,54 @@
-// $(document).ready(function () {
-/*--Selection--*/
-// let inputRandom = 'WHAT';
-//  let inputDate = 'DATE';
-//  let inputState = 'STATE';
-
-//  let url = 'https://covidtracking.com/api/v1/states/current.json' + inputRandom + inputDate + inputLocation;
-//  let p = '';
-//  let loadMore; 
-// fetchData(url);
-
 const url = 'https://covidtracking.com/api/v1/states/current.json';
 const inputRandom = "WHAT";
 const inputDate = "DATE";
 const inputState = "STATE";
 
 $(document).ready(() => {
-    $('.js-search').on('submit', (e) => {
-        let btnSearch = $('.btn-search').val();
-        getState(btn - search);
-        e.preventDefault();
-    });
-});
 
-function getState(btnSearch) {
-    axios.get(Url + btnSearch + inputRandom + inputDate + inputState)
-        .then(function(response) {
-                console.log(response);
-                let covid = response.data.Search;
-                let output = '';
-                $.each(state, (index, state) => {
-                    output += `
-                  <div class = "col-md-3">
-                   
-                  `;
-                });
-                $(state).html(output);
+});
+var array = new Array();
+var arrayImages = new Array();
+
+function getData() {
+
+    var e = document.getElementById("dropdown");
+    var states = e.options[e.selectedIndex].value;
+    if (array.length > 0) {
+        for (i = 0; i < array.length; i++) {
+            if (array[i].state == states) {
+                $('.card-title').html("<h3> Positives are " + array[i].state + " " + array[i].positive + "</h3>")
+                $('.card-body').html("<p class='lead'> " + array[i].lastUpdateEt + "</p>")
             }
+        }
+        for (i = 0; i < arrayImages.length; i++) {
+            if (arrayImages[i].state == states) {
+                $('.card-body').html("<img class='img-fluid' src='" + arrayImages[i].url + "'/>")
+            }
+        }
+    } else {
+        $.get('https://covidtracking.com/api/v1/states/current.json', function(response) {
+
+            array = response;
+            let output = '';
+            var obj;
+            for (i = 0; i < response.length; i++) {
+                if (response[i].state == states) {
+                    $('.card-title').html("<h3> Positives are " + response[i].state + " " + response[i].positive + "</h3>")
+                    $('.card-footer').html("<p class='lead'> " + response[i].lastUpdateEt + "</p>")
+                }
+            }
+
+        })
+        $.get('https://covidtracking.com/api/v1/states/screenshots.json', function(response) {
+
+            arrayImages = response;
+            let output = '';
+            var obj;
+            for (i = 0; i < response.length; i++) {
+                if (response[i].state == states) {
+                    $('.card-body').html("<img class='img-fluid' src='" + response[i].url + "'/>")
+                }
+            }
+        })
+    }
+}
